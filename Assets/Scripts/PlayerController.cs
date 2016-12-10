121d176f126743b68aa170348ddf8c45ee6f638d
Ben Humphries <ben_humphries@caryacademy.org>
2016-12-09 22:10:58 -0500

    Basic Movement
    Added basic player movement based on world coordinates and rounding to
    the nearest tile value.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//If left mouse button is pressed
+		if (Input.GetMouseButtonDown (0)) {
+
+			/*MOVEMENT*/
+
+			//Get mouse position and convert it to world coordinates.
+			Vector2 mouseClick = Input.mousePosition;
+			mouseClick = Camera.main.ScreenToWorldPoint(mouseClick);
+
+			//If coordinates are negative, subtract one as when casted to an int, a negative number will round towards zero.
+			if(mouseClick.x < 0)
+				mouseClick.x --;
+			if(mouseClick.y < 0)
+				mouseClick.y --;
+
+			//Round to nearest tile value and update player position.
+			Vector2 mousePosition = new Vector2 ((int)mouseClick.x + 0.5f, (int)mouseClick.y + 0.5f);
+			transform.position = mousePosition;
+
+		}
+	}
+
+}
